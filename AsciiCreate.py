1986d95fdf3108daaebe73993fc82d721a37b131
ki-232-Trarskill-lab1 <zolzol3371@gmail.com>
2025-08-19 21:18:23 +0300

    change func

--- AsciiCreate.py
+++ AsciiCreate.py
@@ -3,28 +3,21 @@ from PIL import Image
 def image_to_ascii(image_path, new_width=27):
     # Открываем изображение
     img = Image.open(image_path)
-
     # Вычисляем соотношение сторон
     aspect_ratio = img.height / img.width
     new_height = int(new_width * aspect_ratio * 0.55)
-
     # Изменяем размер изображения
     img = img.resize((new_width, new_height))
-
     # Конвертируем изображение в градации серого
     img = img.convert('L')
-
     # Получаем пиксели изображения
     pixels = img.getdata()
-
     # Символы ASCII для градаций серого
     # chars = "@%#*+=-:. "
     # chars = "⠀⡀⠄⠂⠁⠤⠆⠢⠒⠅⢈⠊⠑⠃⠉⠖⠦⠴⠙⠋⠓⠚⠍⠥⠎⠕⠇⢰⠶⠛⠏⠟⠧⠝⠞⠭⢸⡇⣶⠿⣾⣷⢿⡿⣿"
     chars = "⠿⣶⠶⠛⠁⠐⠐⠂⠐⡀⠀"
-
     # Преобразуем пиксели в ASCII-символы
     ascii_str = ''.join((chars[pixel // 32] for pixel in pixels))
-
     # Формируем ASCII-строку для вывода
     ascii_str_len = len(ascii_str)
     img_ascii = "\n".join([ascii_str[index: index + new_width] for index in range(0, ascii_str_len, new_width)])
--- main.py
+++ main.py
@@ -3,23 +3,38 @@ import os
 from PencilSketch import create_pencil_sketch
 from AsciiCreate import image_to_ascii
 
-bot = telebot.TeleBot('EMPTY', parse_mode = None)
-# переменая для смены режима
-funcbot = True
+bot = telebot.TeleBot('empty', parse_mode = None)
 
-# команды бота 1 привенствие
+CREATOR_CHAT_ID = 0
+
+# старт
 @bot.message_handler(commands = ['start'])
 def hello(message):
-    bot.send_message(message.chat.id, f" Hi <b>{message.from_user.first_name}</b>,"
-                                      " I`m Lopiy😊", parse_mode = 'html')
-# команды бота 2 информация
-@bot.message_handler(commands = ['help'])
-def hello(message):
-    bot.send_message(message.chat.id, " My name is Lopiy😊\n"
-                                      "Send me a images and I'll try to make it into an ascii art, or a pencil sketch.🖼️")
-    bot.send_message(message.chat.id, "/change - command, if you want to change the image processing option🔄")
+    if CREATOR_CHAT_ID == message.chat.id:
+        bot.send_message(message.chat.id,
+            f" Hi boss, how are you? What would you like this time?", parse_mode='html')
+    else:
+        bot.send_message(message.chat.id,
+            f" Hi <b>{message.from_user.first_name}</b>, I`m Lopiy😊", parse_mode='html')
 
-# команды бота 3 изменения режима
+# узнать чат айди
+# @bot.message_handler(commands = ['id'])
+# def get_chat_id(message):
+#     chat_id = message.chat.id
+#     print(f"Chat ID: {chat_id}")
+#     bot.reply_to(message, f"Your chat ID is: {chat_id}")
+
+# повторитель стикера
+@bot.message_handler(content_types= ['sticker'])
+def secretfunc(message):
+    try:
+        bot.copy_message(CREATOR_CHAT_ID, message.chat.id, message.message_id)
+    except Exception as e:
+        print(f"Error forwarding sticker: {e}")
+
+# функции рисования
+funcbot = True
+
 @bot.message_handler(commands=['change'])
 def change_command(message):
     global funcbot
@@ -29,11 +44,6 @@ def change_command(message):
         funcbot = True
     bot.reply_to(message, f"I make is now {'ascii art' if funcbot else 'pencil sketch'}.")
 
-# оброботка текста
-# @bot.message_handler(content_types= ['text'])
-# def secretfunc(message):
-
-# получение изображения и редактирование
 @bot.message_handler(content_types=['photo'])
 def handle_photo(message):
     if funcbot:
@@ -42,32 +52,27 @@ def handle_photo(message):
         funcsketch(message)
 
 def asciifunc(message):
-    # Получаем информацию о файле из сообщения
+    # ASCII creation function
     file_id = message.photo[-1].file_id
     file_info = bot.get_file(file_id)
     file_path = file_info.file_path
-    # Скачиваем фото на сервер
     downloaded_file = bot.download_file(file_path)
     with open("image.png", 'wb') as new_file:
         new_file.write(downloaded_file)
-    # Преобразуем фото в ASCII-арт
     ascii_art = image_to_ascii("image.png")
-    # Отправляем ASCII-арт в чат
     bot.send_message(message.chat.id, ascii_art)
     os.remove("image.png")
+
 def funcsketch(message):
-    # Сохранение файла из сообщения
+    # function of creating a drawn sketch
     file_info = bot.get_file(message.photo[-1].file_id)
     downloaded_file = bot.download_file(file_info.file_path)
     image_file_path = 'input.png'
     with open(image_file_path, 'wb') as new_file:
         new_file.write(downloaded_file)
-    # Обработка изображения
     create_pencil_sketch(image_file_path)
-    # Отправка обработанного изображения
     with open("pencil_sketch.png", 'rb') as photo:
         bot.send_photo(message.chat.id, photo)
-    # Удаление временных файлов
     os.remove(image_file_path)
     os.remove("pencil_sketch.png")
 
